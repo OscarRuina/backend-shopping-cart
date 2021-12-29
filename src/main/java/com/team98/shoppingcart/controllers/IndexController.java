ad644cb1e48916bce8f6cbe451688c5ae04aa656
Oscar <oscarruina@hotmail.com>
2021-12-29 17:48:58 -0300

    adding index controller for testing

---
+++ src/main/java/com/team98/shoppingcart/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.team98.shoppingcart.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "Working deployment with heroku";
+    }
+
+}
