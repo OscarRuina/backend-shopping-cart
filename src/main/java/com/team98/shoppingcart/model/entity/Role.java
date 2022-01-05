f2710f0297a12bab7a1c6b8f02f992352627a884
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2022-01-05 12:51:07 -0300

    CDC-1: adding CDC-1 and CDC-3, pom modified (#1)
    * CDC-1: adding CDC-1 and CDC-3, pom modified
    
    * CDC-1: User entity modified
    
    * fixing english words
    
    * CDC-1: fixing english words
    
    Co-authored-by: Joaquin Aman <Joaquin Aman>

---
+++ src/main/java/com/team98/shoppingcart/model/entity/Role.java
@@ -0,0 +1,39 @@
+package com.team98.shoppingcart.model.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ROLES")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ROLES_ID")
+  private Long id;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "DESCRIPTION", nullable = true)
+  private String description;
+
+  @CreationTimestamp
+  @Column(name = "TIMESTAMP", nullable = true)
+  private Timestamp timestamp;
+
+}
---
+++ src/main/java/com/team98/shoppingcart/model/entity/User.java
@@ -0,0 +1,126 @@
+package com.team98.shoppingcart.model.entity;
+
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Setter(AccessLevel.NONE)
+  @Column(name = "USERS_ID")
+  private Long id;
+
+  @Column(name = "FIRST_NAME", nullable = false)
+  private String firstName;
+
+  @Column(name = "LAST_NAME", nullable = false)
+  private String lastName;
+
+  @Column(name = "EMAIL", unique = true, nullable = false)
+  private String email;
+
+  @Column(name = "PASSWORD", nullable = false)
+  private String password;
+
+  @Column(name = "PASSWORD_CONFIRMATION", nullable = false)
+  private String passwordConfirmation;
+
+  @Column(name = "ADDRESS", nullable = false)
+  private String address;
+
+  @Column(name = "DELIVERY_DIRECTION", nullable = false)
+  private String deliveryDirection;
+
+  @Column(name = "PHONE", nullable = false)
+  private int phone;
+
+  @Column(name = "CITY", nullable = false)
+  private String city;
+
+  @Column(name = "STATE", nullable = false)
+  private String state;
+
+  @Column(name = "COUNTRY", nullable = false)
+  private String country;
+
+  @Column(name = "POST_CODE", nullable = false)
+  private String postCode;
+
+  // token reset password(cuando tengamos seguridad)
+
+  @Column(name = "PHOTO", nullable = true)
+  private String photo;
+
+  @JoinColumn(name = "ROLES_ID")
+  @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+  private List<Role> roles;
+
+  @CreationTimestamp
+  @Column(name = "TIMESTAMP", nullable = true)
+  private Timestamp timestamp;
+
+  @Column(name = "SOFT_DELETED", nullable = true)
+  private boolean softDeleted;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return this.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName()))
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return !this.softDeleted;
+  }
+
+}
