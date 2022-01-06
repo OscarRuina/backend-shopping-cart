bdfe03586cf7370095b8cba3fdac36ee28ee0184
Joaquin Aman <Joaquin Aman>
2022-01-06 17:58:40 -0300

    CDC-3: adding role enum

---
+++ src/main/java/com/team98/shoppingcart/config/ApplicationRole.java
@@ -0,0 +1,20 @@
+package com.team98.shoppingcart.config;
+
+public enum ApplicationRole {
+  USER("USER"), ADMIN("ADMIN");
+
+  private static final String ROLE_PREFIX = "ROLE_";
+  private final String name;
+
+  ApplicationRole(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getFullRoleName() {
+    return ROLE_PREFIX + name;
+  }
+}
