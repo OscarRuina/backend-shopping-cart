92e9b83a18f492b4c246c0c77681bfedcdfb5e03
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2022-01-15 11:08:17 -0300

    CDC-4: annotations added to repo & service

--- src/main/java/com/team98/shoppingcart/repository/IUserRepository.java
+++ src/main/java/com/team98/shoppingcart/repository/IUserRepository.java
@@ -2,7 +2,9 @@ package com.team98.shoppingcart.repository;
 
 import com.team98.shoppingcart.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
 
 }
